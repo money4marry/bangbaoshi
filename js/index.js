ca4362ce63335fb9842329940346dc28903f6f18
Frank <wangyang@klicen.com>
2018-05-17 15:00:08 +0800

    完成版

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: true,
+
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // 如果需要前进后退按钮
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    })
+})
